135be8e61458f13eb82e4d9fa2823dee5e68fbd4
Caio André <caio_andre15@hotmail.com>
2021-10-07 23:24:59 -0300

    Implementing API verbs

--- 03_RestWithASP-NET5_Verbos/RestWithASP-NET5/RestWithASP-NET5/Controllers/PersonController.cs
+++ 03_RestWithASP-NET5_Verbos/RestWithASP-NET5/RestWithASP-NET5/Controllers/PersonController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestWithASP_NET5.Model;
+using RestWithASP_NET5.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,15 +15,47 @@ namespace RestWithASP_NET5.Controllers
     {
         private readonly ILogger<PersonController> _logger;
 
-        public PersonController(ILogger<PersonController> logger)
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
         }
 
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult GetSum(string firstNumber, string secondNumber)
+        [HttpGet]
+        public IActionResult Get()
         {
-            return BadRequest("Invalid Input");
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
         }
 
     }
