3df22283bf2cffc3687f8934034c12e53a08fa75
Caio André <caio_andre15@hotmail.com>
2021-10-13 08:10:43 -0300

    Adding versioning

---
+++ 05_RestWithASP-NET5_Versionamento/RestWithASP-NET5/RestWithASP-NET5/Services/Implementations/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASP_NET5.Model;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+        Person Update(Person person);
+
+        void Delete(long Id);
+    }
+}
