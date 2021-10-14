5551a4276622936104ae937741567ce37b398e0a
Caio André <caio_andre15@hotmail.com>
2021-10-14 07:50:21 -0300

    Implementing Migrations

---
+++ 07_RestWithASP-NET5_Migrations/RestWithASP-NET5/RestWithASP-NET5/Repository/IPersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASP_NET5.Model;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5.Repository
+{
+    public interface IPersonRepository    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+        Person Update(Person person);
+
+        void Delete(long Id);
+
+        bool Exists(long id);
+    }
+}
