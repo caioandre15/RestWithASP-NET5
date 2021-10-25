fed9d3eeeaf6c480967de6095ad6148a47f95aa7
Caio André <caio_andre15@hotmail.com>
2021-10-25 08:32:22 -0300

    Implementing Content Negotiation

---
+++ 11_RestWithASP-NET5_ContentNegociation/RestWithASP-NET5/RestWithASP-NET5/Business/Implementations/BooksBusinessImplementation.cs
@@ -0,0 +1,55 @@
+﻿using RestWithASP_NET5.Data.Converter.Implementations;
+using RestWithASP_NET5.Data.VO;
+using RestWithASP_NET5.Model;
+using RestWithASP_NET5.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5.Business.Implementations
+{
+    public class BooksBusinessImplementation : IBooksBusiness
+    {
+        private readonly IRepository<Books> _repository;
+
+        private readonly BooksConverter _converter;
+
+        public BooksBusinessImplementation(IRepository<Books> repository)
+        {
+            _repository = repository;
+            _converter = new BooksConverter();
+        }
+
+        // Method responsible for returning all people
+        public List<BooksVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        // Method responsible for returning one person by ID
+        public BooksVO FindByID(long id)
+        {
+            return _converter.Parse(_repository.FindByID(id));
+        }
+
+        // Method responsible to create one new person
+        public BooksVO Create(BooksVO books)
+        {
+            var booksEntity = _converter.Parse(books);
+            booksEntity = _repository.Create(booksEntity);
+            return _converter.Parse(booksEntity);
+        }
+
+        // Method responsible for updating one person
+        public BooksVO Update(BooksVO books)
+        {
+            var booksEntity = _converter.Parse(books);
+            booksEntity = _repository.Update(booksEntity);
+            return _converter.Parse(booksEntity);
+        }
+
+        // Method responsible for deleting a person from an ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
