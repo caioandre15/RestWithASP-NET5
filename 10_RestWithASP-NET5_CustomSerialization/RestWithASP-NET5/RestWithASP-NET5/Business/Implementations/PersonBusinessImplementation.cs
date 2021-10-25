0fa4572f14a6bdac7e6acbe72d96bf936132b18e
Caio André <caio_andre15@hotmail.com>
2021-10-25 08:05:04 -0300

    Exemplifying Custom Serialization

---
+++ 10_RestWithASP-NET5_CustomSerialization/RestWithASP-NET5/RestWithASP-NET5/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,55 @@
+﻿using RestWithASP_NET5.Data.Converter.Implementations;
+using RestWithASP_NET5.Data.VO;
+using RestWithASP_NET5.Model;
+using RestWithASP_NET5.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+
+        private readonly PersonConverter _converter;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _converter = new PersonConverter();
+        }
+
+        // Method responsible for returning all people
+        public List<PersonVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        // Method responsible for returning one PersonVO by ID
+        public PersonVO FindByID(long id)
+        {
+            return _converter.Parse(_repository.FindByID(id));
+        }
+
+        // Method responsible to create one new person
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        // Method responsible for updating one person
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        // Method responsible for deleting a person from an ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ 10_RestWithASP-NET5_CustomSerialization/RestWithASP-NET5/RestWithASP-NET5/Data/VO/BooksVO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RestWithASP_NET5.Data.VO
+{
+    public class BooksVO
+    {
+        public long Id { get; set; }
+
+        public string Author { get; set; }
+     
+        public DateTime LaunchDate { get; set; }
+     
+        public Decimal Price { get; set; }
+     
+        public string title { get; set; }
+    }
+}
