6fd636703e975ed5c21d78412e358459a37808e3
Caio André <caio_andre15@hotmail.com>
2021-10-28 08:51:30 -0300

    Enabling Colors

---
+++ 14_RestWithASP-NET5_CORS/RestWithASP-NET5/RestWithASP-NET5/Data/Converter/Implementations/BooksConverter.cs
@@ -0,0 +1,45 @@
+﻿using RestWithASP_NET5.Data.Converter.Contract;
+using RestWithASP_NET5.Data.VO;
+using RestWithASP_NET5.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASP_NET5.Data.Converter.Implementations
+{
+    public class BooksConverter : IParser<BooksVO, Books>, IParser<Books, BooksVO>
+    {
+        public Books Parse(BooksVO origin)
+        {
+            if (origin == null) return null;
+            return new Books
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LaunchDate = origin.LaunchDate,
+                Price = origin.Price,
+                title = origin.title
+            };
+        }
+        public BooksVO Parse(Books origin)
+        {
+            if (origin == null) return null;
+            return new BooksVO
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LaunchDate = origin.LaunchDate,
+                Price = origin.Price,
+                title = origin.title
+            };
+        }
+
+        public List<Books> Parse(List<BooksVO> origin)
+        {
+            return origin.Select(item => Parse(item)).ToList();
+        }
+        public List<BooksVO> Parse(List<Books> origin)
+        {
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
---
+++ 14_RestWithASP-NET5_CORS/RestWithASP-NET5/RestWithASP-NET5/Data/VO/PersonVO.cs
@@ -0,0 +1,20 @@
+﻿using RestWithASP_NET5.HyperMedia;
+using RestWithASP_NET5.HyperMedia.Abstract;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5.Data.VO
+{
+    public class PersonVO : ISupportsHyperMedia
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
